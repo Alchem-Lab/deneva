1f845ed1ec2462ba754677e59ac043e4a2864681
Chao Wang <albertwang87@gmail.com>
2019-03-21 00:43:27 -0700

    added test_each.py

---
+++ run/test_each.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import os
+
+use_rdma = 0
+node_cnt = 2
+workload = 'PPS'
+cc_alg = 'WAIT_DIE'
+
+os.system('sed -i \'s/^#define USE_RDMA.*$/#define USE_RDMA ' + str(use_rdma) + '/\' ../config.h')
+os.system('sed -i \'s/^#define NODE_CNT.*$/#define NODE_CNT ' + str(node_cnt) + '/\' ../config.h')
+os.system('sed -i \'s/^#define WORKLOAD.*$/#define WORKLOAD ' + workload + '/\' ../config.h')
+os.system('sed -i \'s/^#define CC_ALG.*$/#define CC_ALG ' + cc_alg + '/\' ../config.h')
+os.system('cd .. && ./build.sh')
+os.system('./run_slurm.sh ' + str(node_cnt + 1) + ' ' +  str(node_cnt))
